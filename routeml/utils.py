2f13cc3f52cb522f3c30397066ec6930ba0b0dc3
Samuel Chin <samuelchin91@gmail.com>
2023-05-19 12:03:14 -0400

    Add integration tests

--- routeml/utils.py
+++ routeml/utils.py
@@ -22,6 +22,30 @@ def routes_to_solution(routes):
         solution += route
     return solution
 
+def add_depot_to_routes(routes):
+    """
+    Adds the depot node to the beginning and end of each route.
+
+    Args:
+        routes (list): List of routes, where each route is a list of nodes.
+
+    Returns:
+        list: List of routes, where each route is a list of nodes, with the depot added.
+
+    Raises:
+        ValueError: If routes is not a list or a nested list.
+    """
+    if not isinstance(routes, list):
+        raise ValueError("Routes must be a list")
+
+    for route in routes:
+        if not isinstance(route, list):
+            raise ValueError("Routes must be a nested list")
+
+        route.insert(0, 0)
+        route.append(0)
+
+    return routes
 
 def solution_to_routes(solution):
     """
@@ -45,6 +69,24 @@ def solution_to_routes(solution):
 
 
 def parse_vrplib_file(source):
+    """
+    Parses a VRPLIB file and returns a dictionary with the following keys:
+    - name: Name of the problem.
+    - comment: Comment of the problem.
+    - type: Type of the problem.
+    - dimension: Number of nodes.
+    - edge_weight_type: Type of edge weight.
+    - capacity: Capacity of the vehicles.
+    - node_coords: Dict of node coordinates.
+    - demand: Dict of node demands.
+    - depot_ids: List of depot IDs.
+
+    Args:
+        source (str): URL or path to the VRPLIB file.
+
+    Returns:
+        dict: Dictionary with the parsed data.
+    """
     if source.startswith('http://') or source.startswith('https://'):
         response = requests.get(source)
         content = response.text
@@ -85,8 +127,8 @@ def parse_vrplib_file(source):
         node_coords_str = node_coords_match.group(1).strip()
         node_coords = {}
         for line in node_coords_str.split('\n'):
-            node_id, x, y = re.findall(r'(\d+)\s+(\d+)\s+(\d+)', line)[0]
-            node_coords[int(node_id)] = (int(x), int(y))
+            node_id, x, y = re.findall(r'(\d+)\s+([-+]?\d*\.?\d+)\s+([-+]?\d*\.?\d+)', line)[0]
+            node_coords[int(node_id) - 1] = (float(x), float(y))
         data['node_coords'] = node_coords
 
     # Extracting demand information
@@ -96,7 +138,7 @@ def parse_vrplib_file(source):
         demand = {}
         for line in demand_section_str.split('\n'):
             node_id, demand_val = re.findall(r'(\d+)\s+(-?\d+)', line)[0]
-            demand[int(node_id)] = int(demand_val)
+            demand[int(node_id) - 1] = int(demand_val)
         data['demand'] = demand
 
     # Extracting depot information
@@ -145,13 +187,14 @@ def parse_vrplib_solution(source):
 
     return routes, cost
 
-def get_cvrp_cost(routes_or_solution, coordinates):
+def get_cvrp_cost(routes_or_solution, coordinates, uchoa=False):
     """
     Compute the total cost of a CVRP solution.
 
     Args:
         routes_or_solution (list or solution): List of routes or a solution.
         coordinates (dict): Dictionary containing node IDs as keys and corresponding coordinates as values.
+        uchoa (bool): Whether to round the distances to the nearest integer. Follows the 2014 paper.
 
     Returns:
         float: Total cost of the CVRP solution.
@@ -170,6 +213,8 @@ def get_cvrp_cost(routes_or_solution, coordinates):
         coord1 = coordinates[node1]
         coord2 = coordinates[node2]
         distance = math.dist(coord1, coord2)
+        if uchoa:
+            distance = round(distance)
         total_cost += distance
 
     return total_cost
