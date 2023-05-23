e069d46c72224460ed95e44f6a92157a9f437405
Samuel Chin <samuelchin91@gmail.com>
2023-05-23 14:44:49 -0400

    Add import statements to __init__

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='routeml',
-    version='0.2.0',
+    version='0.3.0',
     description='Python package for CVRP utilities',
     author='Your Name',
     author_email='jkschin@mit.edu',
