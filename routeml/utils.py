Fix logit masking.

routeml/utils.py
@@ -436,8 +436,8 @@ def get_logit_mask(sol, demands, capacity):
         mask (np.ndarray): the logit mask.
     """
     prob = set(sol)
-    mask = np.full((len(sol), len(prob)), 0.0) # mark all as valid
-    for i in range(len(sol)): # NOTE this loop is 100% correct, don't change this.
+    mask = np.full((len(sol) - 1, len(prob)), 0.0) # mark all as valid
+    for i in range(len(sol) - 1): # NOTE this loop is 100% correct, don't change this.
         # a is all nodes that I visited
         a = set(sol[:i+1])
         if sol[i] != 0:

tests/unit/utils/test_get_logit_mask.py
@@ -11,8 +11,22 @@ class TestGetLogitMask(unittest.TestCase):
                 [0., -np.inf, 0., 0., 0.],
                 [0., -np.inf, -np.inf, -np.inf, -np.inf],
                 [-np.inf, -np.inf, -np.inf, 0., 0.],
+                [0., -np.inf, -np.inf, -np.inf, 0.]])
+
+        result_mask = get_logit_mask(sol, demands, capacity)
+
+        np.testing.assert_array_equal(result_mask, expected_mask)
+
+    def test_get_logit_mask_2(self):
+        sol = [0, 1, 2, 0, 3, 4, 0]
+        demands = np.array([0, 5, 8, 3, 6])
+        capacity = 15
+        expected_mask = np.array([[-np.inf, 0., 0., 0., 0.],
+                [0., -np.inf, 0., 0., 0.],
+                [0., -np.inf, -np.inf, -np.inf, -np.inf],
+                [-np.inf, -np.inf, -np.inf, 0., 0.],
                 [0., -np.inf, -np.inf, -np.inf, 0.],
-                [0., -np.inf, -np.inf, -np.inf, -np.inf]])
+                [0., -np.inf, -np.inf, -np.inf, -np.inf],])
 
         result_mask = get_logit_mask(sol, demands, capacity)
 